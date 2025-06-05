131f660e510114767858af3095a2758db7961c10
Saurtron <saurtron@riseup.net>
2025-06-05 22:55:27 +0200

    Remove comment.

--- rts/Lua/LuaHandleSynced.cpp
+++ rts/Lua/LuaHandleSynced.cpp
@@ -2296,7 +2296,6 @@ int CSplitLuaHandle::LoadStringData(lua_State* L)
 
 	auto handle = CLuaHandle::GetHandle(L);
 
-	// BLOCK HERE
 	if (luaL_loadbuffer_privileged(L, str, len, chunkname, handle->GetDevMode()) != 0) {
 		lua_pushnil(L);
 		lua_insert(L, -2);
